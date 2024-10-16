0ebe2a7120030dca3f505544ac77ae3b0ce728ba
michelecurti <michele.curti@gmail.com>
2024-10-16 17:09:51 +0200

    search for multiple cameras at startup

--- surveillance.py
+++ surveillance.py
@@ -1,19 +1,41 @@
 import os
-import time
-from camera import Camera
-from detect import Detect
 
 # set FFMPEG environment variables to use hardware encoding acceleration
 os.environ["OPENCV_FFMPEG_WRITER_OPTIONS"] = "vcodec;h264_qsv"
 
-#camera = Camera("../asd.mp4", "/surveillance/")
-camera = Camera(0, "/surveillance/")
+# disable opencv errors trying to open unexisting cameras, remove in debug
+os.environ["OPENCV_LOG_LEVEL"]="FATAL"
+
+import cv2
+import time
+from camera import Camera
+from detect import Detect
+
+cameras = []
+
+if False:
+    cameras += [Camera("../asd.mp4", "/surveillance/")]
+else:
+    for i in range (0, 4):
+        cap = cv2.VideoCapture(i)
+        if cap is not None and cap.isOpened():
+            cap.release()
+            print("Found camera " + str(i))
+            cameras += [Camera(i, "/surveillance/")]
+
 detect = Detect("/surveillance/detect/", Detect.TYPE_BODY)
 
+curr = 0
 while True:
     time.sleep(0.5)
-    valid, frame = camera.last_frame()
+    valid, frame = cameras[curr].last_frame()
     if valid:
         detect.frame(frame)
+    curr += 1
+    if curr >= len(cameras):
+        curr = 0
 
-camera.exit()
+for camera in cameras:
+    camera.exit()
+
+detect.exit()
